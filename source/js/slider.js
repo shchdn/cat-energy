7af44a69a181b539d3fce1e65004db93212c8700
shchdn <shchedrin.georgy@gmail.com>
2020-10-06 00:46:59 +0300

    range slider is ready (need to update mobile and add animations)

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,21 +1,27 @@
 $(function () {
     const grip = $('.slider__grip'),
-          scale = $('.slider__scale');
+          scale = $('.slider__scale'),
+          photoBefore = $('.slider__photo--before'),
+          photoAfter = $('.slider__photo--after');
     function onGripMouseDown(e){
         e.preventDefault();
         const oldPos = e.clientX,
-              scalePos = parseInt(grip.css('margin-left')),
-              scaleWidth = scale.width();
+              scaleWidth = scale.width(),
+              scalePos = parseInt(grip.css('margin-left')) / (scaleWidth / 100);
         $('body').on('mousemove', onGripMouseMove(oldPos, scalePos, scaleWidth));
         $('body').on('mouseup', onGripMouseUp);
     }
     function onGripMouseMove(oldPos, scalePos, scaleWidth){
         return function(e){
             let shift = oldPos - e.clientX,
-                marginValue = scalePos - shift;
-            if (marginValue < 0) marginValue = 0;
-            if (marginValue > scaleWidth) marginValue = scaleWidth;
-            grip.css('margin-left', `${marginValue}px`)    
+                widthValue = (shift / (scaleWidth / 100));
+            if (widthValue < 0 && widthValue < scalePos - 100) widthValue = scalePos - 100;
+            if (widthValue > 0 && widthValue > scalePos) widthValue = scalePos;
+            
+
+            grip.css('margin-left', `${scalePos - widthValue}%`)    
+            photoBefore.css('width', `${100 - (scalePos - widthValue)}%`)    
+            photoAfter.css('width', `${scalePos - widthValue}%`)
         }
     }
     function onGripMouseUp(){
