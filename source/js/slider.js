51401ce4697e2ab4b5d755f9e7c82d07488c085c
georgy <shchedrin.georgy@gmail.com>
2020-10-06 21:35:50 +0300

    slider finished

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,6 +1,7 @@
 $(function () {
     const grip = $('.slider__grip'),
           scale = $('.slider__scale'),
+          mobile = $('.slider__mobile'),
           photoBefore = $('.slider__photo--before'),
           photoAfter = $('.slider__photo--after');
     function onGripMouseDown(e){
@@ -14,14 +15,13 @@ $(function () {
     function onGripMouseMove(oldPos, scalePos, scaleWidth){
         return function(e){
             let shift = oldPos - e.clientX,
-                widthValue = (shift / (scaleWidth / 100));
-            if (widthValue < 0 && widthValue < scalePos - 100) widthValue = scalePos - 100;
-            if (widthValue > 0 && widthValue > scalePos) widthValue = scalePos;
-            
-
-            grip.css('margin-left', `${scalePos - widthValue}%`)    
-            photoBefore.css('width', `${100 - (scalePos - widthValue)}%`)    
-            photoAfter.css('width', `${scalePos - widthValue}%`)
+                shiftPercent = (shift / (scaleWidth / 100));
+            if (shiftPercent < 0 && shiftPercent < scalePos - 100) shiftPercent = scalePos - 100;
+            if (shiftPercent > 0 && shiftPercent > scalePos) shiftPercent = scalePos;
+            let width = scalePos - shiftPercent;
+            grip.css('margin-left', `${width}%`)    
+            photoBefore.css('width', `${100 - (width)}%`)    
+            photoAfter.css('width', `${width}%`)
         }
     }
     function onGripMouseUp(){
@@ -29,4 +29,20 @@ $(function () {
         $(this).off('mouseup')
     }
     grip.on('mousedown', onGripMouseDown);
+    mobile.on('click', function(){
+        let leftClass = 'slider__grip-mobile--left',
+            rightClass = 'slider__grip-mobile--right';
+        if(grip.attr('class').split(/\s+/).length == 1 || grip.hasClass(leftClass)){
+            grip.removeClass(leftClass);
+            grip.toggleClass(rightClass);
+            photoBefore.css('width', '0%'); 
+            photoAfter.css('width', '100%');
+        }
+        else{
+            grip.toggleClass(rightClass);
+            grip.toggleClass(leftClass);
+            photoBefore.css('width', '100%');  
+            photoAfter.css('width', '0%');
+        }
+    })
 });
